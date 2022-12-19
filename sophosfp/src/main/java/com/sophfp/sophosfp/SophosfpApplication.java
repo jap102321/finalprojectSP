3576c377c4d2020cfcc06d59bd6515c5c88439d1
Juan Pablo <jupago14@gmail.com>
2022-12-18 20:58:17 -0500

    Adding starting project files

---
+++ sophosfp/src/main/java/com/sophfp/sophosfp/SophosfpApplication.java
@@ -0,0 +1,13 @@
+package com.sophfp.sophosfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SophosfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SophosfpApplication.class, args);
+	}
+
+}
---
+++ sophosfp/src/test/java/com/sophfp/sophosfp/SophosfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sophfp.sophosfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SophosfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
